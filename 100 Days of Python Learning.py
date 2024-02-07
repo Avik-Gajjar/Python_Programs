d3b1dd244dc93e3fac529267b846917af397abc2
Avik Gajjar <avikgjr@meta.com>
2024-02-07 22:09:01 +0000

    2 days of learing Python

---
+++ 100 Days of Python Learning.py
@@ -0,0 +1,29 @@
+# DAY ONE - BAND NAME GENERATOR
+
+# print("Welcome to Band Name Generator")
+# city_name = input("What's the name of the city you grew up in?\n")
+# pet_name = input("What's the name of your pet?\n")
+# print("Your Band name could be: " + city_name + " " + pet_name)
+
+# DAY TWO - TIP CALCULATOR [PEMDAS ** exponential // flow - +=]
+
+# print("Welcome to the tip calculator")
+# total_bill = float(input("What was the total bill? "))
+# tip_perc = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+# members = int(input("How many people to split the bill? "))
+#
+# tip_conv = (tip_perc / total_bill) * 100
+#
+# gross_bill = total_bill + tip_conv
+#
+# final_pay = gross_bill / members
+#
+# total_pay = round(final_pay, 2)
+#
+# print(f"Each person have to pay {total_pay}")
+
+# DAY THREE -
+
+user_name = input("User name")
+sum_up = user_name
+print(sum_up)
