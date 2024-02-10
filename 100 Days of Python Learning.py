1c71f4f93765e54fd84d19d12d9ce07eb02f5989
Avik-Gajjar <avik.gajjar@gmail.com>
2024-02-10 18:55:51 +0000

    Added Day four programs

--- 100 Days of Python Learning.py
+++ 100 Days of Python Learning.py
@@ -82,27 +82,101 @@
 
 # DAY THREE - Treasure Island
 
-print("Welcome to Treasure Island!")
-print("Your Mission is to find the treasure.")
+# print("Welcome to Treasure Island!")
+# print("Your Mission is to find the treasure.")
+#
+# user_input_1 = input("You are at a cross road. Where do you want to go? Type left or right.\n").lower()
+# if user_input_1 == "right":
+#   print("Fall into a hole. Game Over.")
+# elif user_input_1 == "left":
+#   user_input_2 = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
+#   if user_input_2 == "swim":
+#     print("Attacked by Crocodile. Game Over.")
+#   elif user_input_2 == "wait":
+#     user_input_3 = input("You arrive at  the island unharmed. There is house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n").lower()
+#     if user_input_3 == "red":
+#       print("Burned by Fire. Game Over.")
+#     elif user_input_3 == "yellow":
+#       print("You found the Treasure! YOU WIN!!")
+#     elif user_input_3 == "blue":
+#       print("Eaten by beast. Game Over.")
+#     else:
+#       print("You can't choose anything else. Game Over.")
+#   else:
+#     print("Sorry, Wrong Input.")
+# else:
+#   print("Sorry, Wrong Input.")
 
-user_input_1 = input("You are at a cross road. Where do you want to go? Type left or right.\n").lower()
-if user_input_1 == "right":
-  print("Fall into a hole. Game Over.")
-elif user_input_1 == "left":
-  user_input_2 = input("You come to a lake. There is an island in the middle of the lake. Type 'wait' to wait for a boat. Type 'swim' to swim across.\n").lower()
-  if user_input_2 == "swim":
-    print("Attacked by Crocodile. Game Over.")
-  elif user_input_2 == "wait":
-    user_input_3 = input("You arrive at  the island unharmed. There is house with 3 doors. One red, one yellow, and one blue. Which color do you choose?\n").lower()
-    if user_input_3 == "red":
-      print("Burned by Fire. Game Over.")
-    elif user_input_3 == "yellow":
-      print("You found the Treasure! YOU WIN!!")
-    elif user_input_3 == "blue":
-      print("Eaten by beast. Game Over.")
-    else:
-      print("You can't choose anything elese. Game Over.")
-  else:
-    print("Sorry, Wrong Input.")
-else:
-  print("Sorry, Wrong Input.")
+# DAY FOUR - Treasure Map
+
+# line1 = ["⬜️", "️⬜️", "️⬜️"]
+# line2 = ["⬜️", "⬜️", "️⬜️"]
+# line3 = ["⬜️️", "⬜️️", "⬜️️"]
+# map = [line1, line2, line3]
+# print("Hiding your treasure! X marks the spot.")
+# position = input()  # Where do you want to put the treasure?
+# # 🚨 Don't change the code above 👆
+# # Write your code below this row 👇
+# letter = position[0].lower()
+# abc = ["a", "b", "c"]
+# letter_index = abc.index(letter)
+# number_index = int(position[1]) - 1
+# map[number_index][letter_index] = "X"
+#
+# # Write your code above this row 👆
+# # 🚨 Don't change the code below 👇
+# print(f"{line1}\n{line2}\n{line3}")
+#
+# DAY FOUR - Rock, Paper, Scissors
+#
+# import random
+# rock = '''
+#     _______
+# ---'   ____)
+#       (_____)
+#       (_____)
+#       (____)
+# ---.__(___)
+# '''
+#
+# paper = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#           _______)
+#          _______)
+# ---.__________)
+# '''
+#
+# scissors = '''
+#     _______
+# ---'   ____)____
+#           ______)
+#        __________)
+#       (____)
+# ---.__(___)
+# '''
+# game_images = [rock, paper, scissors] # Images list
+#
+# user_input = int(input("What do you choose? Type 0 for Rock, 1 for Paper, 2 for Scissors.\n"))
+# if user_input >= 3 or user_input < 0: # Checking invalid input
+#     print("Incorrect Input. Try Again.")
+# else:
+#     print(game_images[user_input])
+#
+#     comp_input = random.randint(0, 2)
+#     print("Computer Chose:")
+#     print(game_images[comp_input])
+#
+#     if user_input == 0 and comp_input == 2: # User winning conditions
+#         print("You Win!")
+#     elif comp_input == 0 and user_input == 2: # Computer winning conditions
+#         print("You Lose!")
+#     elif comp_input > user_input: # Computer winning conditions
+#         print("You Lose!")
+#     elif user_input > comp_input: # User winning conditions
+#         print("You Win!")
+#     elif comp_input == user_input: # Draw Conditions
+#         print("It's a Draw")
+
+# DAY FIVE
