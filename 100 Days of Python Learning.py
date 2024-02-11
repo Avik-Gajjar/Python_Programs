632e579d75b629ec44da26c3bec11e9aa52d686a
Avik-Gajjar <avik.gajjar@gmail.com>
2024-02-11 23:22:36 +0000

    Added day five programs. Very challenging!

--- 100 Days of Python Learning.py
+++ 100 Days of Python Learning.py
@@ -179,4 +179,86 @@
 #     elif comp_input == user_input: # Draw Conditions
 #         print("It's a Draw")
 
-# DAY FIVE
+# DAY FIVE - Average height - without len and sum function
+
+# Input a Python list of student heights
+# student_heights = input().split()
+# for n in range(0, len(student_heights)):
+#     student_heights[n] = int(student_heights[n])
+# # 🚨 Don't change the code above 👆
+#
+# # Write your code below this row 👇
+# sum_of_height = 0
+# for height_1 in student_heights:
+#     sum_of_height += height_1
+# print(f"total height = {sum_of_height}")
+#
+# number_of_student = 0
+# for students in student_heights:
+#     number_of_student += 1
+# print(f"number of students = {number_of_student}")
+#
+# avg_height = round(sum_of_height / number_of_student)
+# print(f"average height = {avg_height}")
+
+# DAY FIVE - Max Score
+# Input a list of student scores
+# student_scores = input().split()
+# for n in range(0, len(student_scores)):
+#   student_scores[n] = int(student_scores[n])
+#
+# # Write your code below this row 👇
+# highest_score = 0
+# for score in student_scores:
+#     if score >= highest_score:
+#       highest_score = score
+# print(f"The highest score in the class is: {highest_score}")
+
+# DAY FIVE - FizzBuzz Program
+
+# for fizzbuzz in range(1, 101):
+#   if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+#     print("FizzBuzz")
+#   elif fizzbuzz % 3 == 0:
+#     print("Fizz")
+#   elif fizzbuzz % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(fizzbuzz)
+
+# DAY FIVE - PyPASSWORD GENERATOR
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+#
+# print("Welcome to the PyPassword Generator!")
+# nr_letters = int(input("How many letters would you like in your password?\n"))
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+#
+# user_password_list = []  # empty list to add random letter, numbers and symbols
+#
+# for x in range(0, nr_letters):
+#     user_password_list += random.choice(letters)  # random choice will pick up random item from list
+#
+# for y in range(0, nr_symbols):
+#     user_password_list += random.choice(numbers)
+#
+# for z in range(0, nr_numbers):
+#     user_password_list += random.choice(symbols)
+#
+# random.shuffle(user_password_list)  # will shuffle the input from different list
+#
+# final_user_password = ""
+# for char in user_password_list:
+#     final_user_password += char
+#
+# print(final_user_password)
+
+# end="" will show multiple for loop output on same line
+# Eazy Level - Order not randomised:
+# e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+# Hard Level - Order of characters randomised:
+# e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
