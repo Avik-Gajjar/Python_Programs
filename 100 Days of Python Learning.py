106c0b550e46159e4a1469fe1cf2a7ae72254605
Avik-Gajjar <avik.gajjar@gmail.com>
2024-03-10 22:47:09 +0000

    Completed Calculator program.

--- 100 Days of Python Learning.py
+++ 100 Days of Python Learning.py
@@ -576,37 +576,51 @@
 
 from Calculator_art import logo
 
-print(logo)
 
-# Addition
 def add(n1, n2):
    return n1 + n2
 
-# Subtraction
+
 def subtract(n1, n2):
    return n1 - n2
 
-# Multiplication
+
 def multiply(n1, n2):
    return n1 * n2
 
-# Division
+
 def divide(n1, n2):
    return n1 / n2
 
 
-calculation_dict = {"+": add, "-": subtract, "*": multiply, "/": divide}
-
-num1 = int(input("Enter the first number: "))
-
-for items in calculation_dict:
-   print(items)
-operation_symbol = input("Pick an operation from the line above: ")
-
-num2 = int(input("Enter the second number: "))
-
-calculation_function= calculation_dict[operation_symbol]
-result = calculation_function(num1, num2)
+operations = {
+   "+": add,
+   "-": subtract,
+   "*": multiply,
+   "/": divide
+}
 
 
-print(f"{num1} {operation_symbol} {num2} = {result}")
+def calculator():
+   print(logo)
+
+   num1 = float(input("+"))
+   for symbol in operations:
+      print(symbol)
+   should_continue = True
+
+   while should_continue:
+      operation_symbol = input("Pick an operation: ")
+      num2 = float(input("What's the next number?: "))
+      calculation_function = operations[operation_symbol]
+      answer = calculation_function(num1, num2)
+      print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+      if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == 'y':
+         num1 = answer
+      else:
+         should_continue = False
+         calculator()
+
+
+calculator()
